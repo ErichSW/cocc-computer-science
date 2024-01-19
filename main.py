5cd2ef018f7dfa40f9e8d4f380e3052545897e83
ErichSW <91813686+ErichSW@users.noreply.github.com>
2024-01-19 12:16:10 -0800

    Update main.py
    Reorganized and documented

--- main.py
+++ main.py
@@ -1,43 +1,72 @@
-import csv
-import pandas as pd
+# CS161 - Erich, Elijah, and Erika extra credit group project
+# Goals are to view, search, edit, and save an external database
 
-def ssprint():
+# get csv module for interfacing with the external CSV database
+import csv
+# get pandas for easy data manipulation
+import pandas as pd
+# set location of database
+file_path = 'DBEC.csv'
+
+# setting up functions here
+def helps():
+    print("all    = Display entire database.")
+    print("search = Return a row containing a specific line of text.")
+    print("add    = Enter a new row of data into the database.")
+    print("delete = Remove a row of data from the database.")
+    print("exit   = Close the program.")
+
+
+def sheet():
     pd.set_option('display.width', None)
-    file_path = 'DBEC.csv'
     df = pd.read_csv(file_path)
     print(df)
 
-def search_csv(file_path, search_term):
+
+def search(file_path, search_term):
     with open(file_path, 'r') as file:
         csv_reader = csv.reader(file)
-
         headers = next(csv_reader, None)
-
         for row in csv_reader:
             if search_term in row:
                 return row
     return None
 
+
+def new_search():
+    i = 0
+    while i == 0:
+        print("Enter search term, or 'exit'.")
+        search_term = input()
+        if search_term == "exit":
+            i = 1
+        else:
+            result = search(file_path, search_term)
+            if result:
+                print(f"Found: {result}")
+            else:
+                print("Search term not found.")
+
+
 def add_record():
-    print("Type Course Number")
-    course = input()[:5] # limits no. of characters to 5, etc.
-    course = str(course) # makes sure input is always a string
+    print("Type Course Number (5 digits):")
+    course = input()[:5]  # limits no. of characters to 5, etc.
+    course = str(course)  # makes sure input is always a string
     print("Type Course Name:")
     name = input()[:30]
     name = str(name)
     print("Type Number of Credits:")
-    credits = input()[:1]
-    credits = str(credits)
-    print("Type Department Abbreviation:")
+    credit = input()[:1]
+    credit = str(credit)
+    print("Type Department Abbreviation (4 letters:")
     dept = input()[:4]
     dept = str(dept)
-    dept = dept.upper() # makes abbreviation all caps
+    dept = dept.upper()  # makes abbreviation all caps
     print("Type Description (max 100 char):")
     desc = input()[:100]
     desc = str(desc)
-    print(course, name, credits, dept, desc)
-    new_record = {course, name, credits, dept, desc}
-    file_path = 'DBEC.csv'
+    print(course, name, credit, dept, desc)
+    new_record = {course, name, credit, dept, desc}
     with open(file_path, 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(new_record)
@@ -53,39 +82,34 @@ def add_record():
     # print(df)
     # df.to_csv('DBEC.csv', index=False) # save data back into csv when completed
 
+
 def del_record():
-    print("nothing")
+    print("unfinished")
+
 
 # main loop
 d = 0
 while d == 0:
-    print("Type Sheet, Search or Exit")
+    print("Enter command, or 'help'.")
     cmd = input()
     cmd = cmd.lower()
-    # pull whole spreadsheet function
-    if cmd == "sheet":
-        ssprint()
-    # pull search function
+    # summon list of commands
+    if cmd == "help":
+        helps()
+    # show whole sheet
+    elif cmd == "all":
+        sheet()
+    # use search function
     elif cmd == "search":
-        r = 0
-        while r == 0:
-            print("Enter Search Term")
-            file_path = 'DBEC.csv'
-            search_term = input()
-            if search_term == "exit":
-                r = 1
-            else:
-                result = search_csv(file_path, search_term)
-                if result:
-                    print(f"Found: {result}")
-                else:
-                    print("Search term not found.")
-    # exit the loop
+        new_search()
+    # add row to database
     elif cmd == "add":
         add_record()
+    # delete row from database
     elif cmd == "delete":
         del_record()
+    # exit the loop
     elif cmd == "exit":
         d = 1
     else:
-        print("error")
+        print("Invalid command.")
